7a767707cba75e5c4d94215c1c68e2890bf8e03e
MansurAhmed1 <mansurahmed75236@gmail.com>
2022-04-04 19:41:03 +0600

    add-about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,28 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-          
+          <ul>
+              <li>
+               I have created a home page where there is a product .I have mentioned the features of that product
+
+              </li>
+              <li>
+              There is a section for customer reviews
+
+              </li>
+              <li>
+               I have created a review page All the  reviews are shown on the page called Review
+
+              </li>
+              <li>
+              I have created a dashboard page.
+              Where two charts are shown, a line chart and an area chart
+
+              </li>
+              <li>
+               I have created a blog page .Where the answers to the two questions are written
+              </li>
+          </ul>
         </div>
     );
 };
